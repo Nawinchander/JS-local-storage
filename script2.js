ab049f7e8b52ee6ceae237c88997f7294a6517db
Nawin chander <nawinnapzy289@gmail.com>
2026-02-10 21:14:35 +0530

    add files

---
+++ script2.js
@@ -0,0 +1,9 @@
+ git fetch && git pull
+At line:1 char:11
++ git fetch && git pull
++           ~~
+The token '&&' is not a valid statement separator in this version.
+    + CategoryInfo          : ParserError: (:) [], ParentContainsErrorRecordException
+    + FullyQualifiedErrorId : InvalidEndOfLine
+ 
+PS E:\software practise\2026\local storage> 
